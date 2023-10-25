6a574a681425365160dca24b46a9158bf64b9d3b
DIpu <deepuep@gmail.com>
2023-10-25 18:33:26 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/spin_event_2023/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.spin_event_2023
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:spin_event_2023/src/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
