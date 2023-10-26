e8000afe0694850bceab4d4e3f42cd691b5b1729
Arun-pushpan <arunpushpan2018@gmail.com>
2023-10-26 10:13:59 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:spin_event_2023/view/src/spin_wheel/users.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+          apiKey: "AIzaSyBbi104_QooERL_XR3Ne8Lu_yiG6rjvbPY",
+          appId: "1:168388114269:web:e4a65e0dd3b16213725ab4",
+          messagingSenderId: "168388114269",
+          projectId:"reachout-a0f92",
+          measurementId: "G-JMT4Z1E8G0",
+          storageBucket:  "reachout-a0f92.appspot.com",
+        ));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
@@ -25,4 +41,11 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
+// const firebaseConfig = {
+//
+//   // authDomain: "reachout-a0f92.firebaseapp.com",
+//   // storageBucket: "reachout-a0f92.appspot.com",
+//   // measurementId: "G-JMT4Z1E8G0"
+// };
 
