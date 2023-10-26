e66c11c5dcfc63dd85e2d5555bef357237df7a6c
hari appu <appuharikrishnan08@gmail.com>
2023-10-26 10:15:34 +0530

    spin

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:spin_event_2023/view/src/spin_wheel/users.dart';
-
+   
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key});               
 
   // This widget is the root of your application.
   @override
