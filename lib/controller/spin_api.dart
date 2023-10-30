5a3860d297c0bff5258f61f81f36bc1ca4dd14c1
abintclanleo <abintclanleo@gmail.com>
2023-10-30 19:28:20 +0530

    finished logic

--- lib/controller/spin_api.dart
+++ lib/controller/spin_api.dart
@@ -8,39 +8,68 @@ import '../const/firebase_const.dart';
 
 class SpinApi {
   List dbProducts;
+  
+  
+
   SpinApi({required this.dbProducts});
 
   static late int random;
   List<DocumentSnapshot> products = [];
+ List<int> cherrylist = [0,3,6,9];
+  List<int> voucherlist = [1,7];
+  List<int> prevoucherlist = [4,10];
 
-  spinButtonClik() {
+
+  
+ Future spinButtonClik()async {
     print("api page list count==========${dbProducts.length}");
     int produtindex = Fortune.randomInt(0, dbProducts.length);
     print(dbProducts[produtindex].productname);
-    if (dbProducts[produtindex].productname == 'bluetoothSpeaker') {
+    int gamecount=await getGameCount();
+
+    print("game count===========$gamecount");
+     if(gamecount==0){
+       if (dbProducts[produtindex].productname == 'bluetoothSpeaker') {
       d.log('bluetoothSpeaker');
 
+
       random = 8;
+      decrementProductCount('bluetoothSpeaker');
     } else if (dbProducts[produtindex].productname == 'EarBud') {
       d.log('EarBud');
 
       random = 5;
+      decrementProductCount('EarBud');
     } else if (dbProducts[produtindex].productname == 'preVoucher') {
       d.log('prevoucher');
-      random = 4;
+      int randomindex=Random().nextInt(prevoucherlist.length);
+    
+       random=prevoucherlist[randomindex];
+        decrementProductCount('preVoucher');
+     
     } else if (dbProducts[produtindex].productname == 'voucher') {
       d.log('voucher');
-      random = 1;
+
+       int randomindex=Random().nextInt(voucherlist.length);
+      random=voucherlist[randomindex];
+        decrementProductCount('voucher');
     } else if (dbProducts[produtindex].productname == 'watch') {
       d.log('watch');
       random = 2;
+      decrementProductCount("watch");
     }
+     }else{
+      int randomindex=Random().nextInt(cherrylist.length);
+      print("this is the cherry randomindex======$randomindex");
+       random=cherrylist[randomindex];
 
-    // random=1;
-    // if(random==1){
-    //      decrementProductCount('cap');
-    // }
-    decrementCount();
+      print("this is the cherry random======$random");
+      
+     }
+    
+
+   
+    // decrementCount(gamecount);
   }
 
   static Future<void> decrementProductCount(product) async {
@@ -89,23 +118,37 @@ class SpinApi {
         .collection("users")
         .snapshots();
   }
+    static Future<int> getGameCount() async{
+       final docRef = firestore.collection(spinEventCollection).doc(logic);
+       
+           DocumentSnapshot documentSnapshot = await docRef.get();
+          
+              int currentCount = documentSnapshot.get('count');
+             
+               return currentCount;
+           
+     
+       
+    }
 
   static Future<void> decrementCount() async {
     final docRef = firestore.collection(spinEventCollection).doc(logic);
     try {
       DocumentSnapshot documentSnapshot = await docRef.get();
       if (documentSnapshot.exists) {
-        int currentCount = documentSnapshot.get('count2');
-        print(currentCount);
+        int currentCount = documentSnapshot.get('count');
+       
+        print("this is current count============$currentCount");
         int newcount = currentCount - 1;
+        print("==========$newcount");
 
         if (newcount >= 0) {
-          await docRef.update({'count2': newcount});
+          await docRef.update({'count': newcount});
           print('Count decremented successfully.');
         } else {
           // Reset the count to random
           int resetCount = Random().nextInt(5) + 4;
-          await docRef.update({'count2': resetCount});
+          await docRef.update({'count': resetCount});
           print('Count reset to $resetCount.');
         }
       } else {
