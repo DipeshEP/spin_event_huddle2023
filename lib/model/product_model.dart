304a458aab8f0634986941b83658033f4c4d3ea6
Arun-pushpan <arunpushpan2018@gmail.com>
2023-12-15 20:14:05 +0530

    friday

--- lib/model/product_model.dart
+++ lib/model/product_model.dart
@@ -19,7 +19,7 @@ final List<Products> productlist = [
     name: 'Cherry',
     price: "100",
     color: const Color(0xff0067A5),
-    image: const AssetImage("assets/cherry.png"),
+    image: const AssetImage("assets/images (1) (1).png"),
   ),
   Products(
     name: 'Voucher',
@@ -31,13 +31,13 @@ final List<Products> productlist = [
     name: 'Watch',
     price: "300",
     color: const Color(0xff50C878),
-    image: const AssetImage("assets/2.png"),
+    image: const AssetImage("assets/4-6-bxio2003-bxsm2003-bxsm2001-android-ios-beatxp-yes-original-imaguyfzcesjgfrr (1).png"),
   ),
   Products(
     name: 'Cherry',
     price: "400",
     color: const Color(0xff9966CC),
-    image: const AssetImage("assets/cherry.png"),
+    image: const AssetImage("assets/images (1) (1).png"),
   ),
   Products(
     name: 'Voucher',
@@ -55,7 +55,7 @@ final List<Products> productlist = [
     name: 'Cherry',
     price: "700",
     color: const Color(0xffB76E79),
-    image: const AssetImage("assets/cherry.png"),
+    image: const AssetImage("assets/images (1) (1).png"),
   ),
   Products(
     name: 'Voucher',
@@ -73,7 +73,7 @@ final List<Products> productlist = [
     name: 'Cherry',
     price: "1000",
     color: const Color(0xff98FF98),
-    image: const AssetImage("assets/cherry.png"),
+    image: const AssetImage("assets/images (1) (1).png"),
   ),
   Products(
     name: 'Voucher',
